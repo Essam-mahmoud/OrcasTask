cf2d1d266a1eaf2779d23f76f57b3cc7903e1a01
mohamed <mo7am3d427@hotmail.com>
2020-11-05 09:33:13 +0200

    Initial Commit

---
+++ OrcasTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrcasTask
+//
+//  Created by Smart Zone on 11/5/20.
+//  Copyright © 2020 Smart Zone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OrcasTaskTests/OrcasTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  OrcasTaskTests.swift
+//  OrcasTaskTests
+//
+//  Created by Smart Zone on 11/5/20.
+//  Copyright © 2020 Smart Zone. All rights reserved.
+//
+
+import XCTest
+@testable import OrcasTask
+
+class OrcasTaskTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
